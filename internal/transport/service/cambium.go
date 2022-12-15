133eb44a9ffe7372e527ad2d53feb2f635147847
Cleyson Lima <cleysonph@gmail.com>
2022-12-15 10:22:02 -0300

    create the service to get cambium

---
+++ internal/transport/service/cambium.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type CambiumData struct {
+	Code       string `json:"code"`
+	Codein     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+}
+
+type CambiumResponse map[string]CambiumData
+
+func GetCambium(ctx context.Context, from, to string) (*CambiumResponse, error) {
+	url := fmt.Sprintf("https://economia.awesomeapi.com.br/json/last/%s-%s", from, to)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var data CambiumResponse
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
